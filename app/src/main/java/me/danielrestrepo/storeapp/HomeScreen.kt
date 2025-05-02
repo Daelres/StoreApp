4787376b10fed9edb81cd93127b75dc6cd6a9ee1
Daniel Restrepo <Daelres10@gmail.com>
2025-05-01 23:39:47 -0500

    Se añade visualización del correo del usuario que inició sesión

--- app/src/main/java/me/danielrestrepo/storeapp/HomeScreen.kt
+++ app/src/main/java/me/danielrestrepo/storeapp/HomeScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -25,6 +26,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextOverflow
@@ -40,6 +42,7 @@ fun HomeScreen(onClickExit: () -> Unit = {}) {
 
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
     val auth = Firebase.auth
+    val user = auth.currentUser
 
     Scaffold(
         topBar = {
@@ -56,11 +59,18 @@ fun HomeScreen(onClickExit: () -> Unit = {}) {
                     )
                 },
                 navigationIcon = {
-                    IconButton(onClick = { /* do something */ }) {
-                        Icon(
-                            imageVector = Icons.Default.AccountCircle,
-                            contentDescription = "Localized description"
-                        )
+                    Row(verticalAlignment = Alignment.CenterVertically){
+                        IconButton(onClick = { /* do something */ }) {
+                            Icon(
+                                imageVector = Icons.Default.AccountCircle,
+                                contentDescription = "Localized description"
+                            )
+                        }
+                        if (user != null) {
+                            Text(
+                                user.email.toString()
+                            )
+                        }
                     }
                 },
                 actions = {
